a891825508994a51eed0dd31070da275e46e6ae4
Sameer <sameer4363@gmail.com>
2025-02-01 09:34:01 -0400

    Initial commit- pop up on whitelisted site

---
+++ background.js
@@ -0,0 +1,9 @@
+chrome.runtime.onInstalled.addListener(() => {
+  console.log('Extension installed!');
+});
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  if (message.action === 'openPopup') {
+    chrome.action.openPopup();
+  }
+});
