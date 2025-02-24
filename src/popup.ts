9081fd621d60b1518bc8e845f45e19b381a6554e
Sameer <sameer4363@gmail.com>
2025-02-24 18:58:23 -0400

    Add DOM data retrieval on button click

--- src/popup.ts
+++ src/popup.ts
@@ -4,8 +4,18 @@ const clickMeButton = document.getElementById("clickMe");
 const itemTitle = document.getElementById("item-title");
 // const itemInfo = document.getElementById("item-info");
 
+const setTitle = (title: string) => {
+  if (itemTitle) itemTitle.textContent = title;
+};
+
 if (clickMeButton) {
   clickMeButton.addEventListener("click", () => {
+    getActiveTabURL().then((activeTab) => {
+      console.log("activeTab: ", activeTab);
+      if (activeTab && activeTab.id) {
+        chrome.tabs.sendMessage(activeTab.id, { type: "SCAN" }, setTitle);
+      }
+    });
     alert("Button clicked!");
   });
 }
--- src/scripts/content.ts
+++ src/scripts/content.ts
@@ -1,2 +1,14 @@
 chrome.runtime.sendMessage({ action: "openPopup" });
 console.log("Content script running on a whitelisted site.");
+
+(() => {
+  const scanPage = () => {
+    const title = document.querySelector("h1")?.textContent;
+    return title ? title : "no title found";
+  };
+
+  chrome.runtime.onMessage.addListener((msg, sender, response) => {
+    const { type } = msg;
+    if (type === "SCAN") response(scanPage());
+  });
+})();
